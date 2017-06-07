613c305eb6abc08be350173f0d2cb25a7082b688
jhon boo <zbzbhahaee@163.com>
2017-06-07 17:43:16 +0800

    1

---
+++ app/src/main/java/com/zb/app/pandatv/acts/fragment/BaseFragment.java
@@ -0,0 +1,19 @@
+package com.zb.app.pandatv.acts.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by CIA on 2017/6/7.
+ */
+
+public class BaseFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
